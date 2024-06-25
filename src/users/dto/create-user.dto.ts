fda55768fb11bc9df55f6dcc368ca8022eeaa54a
hai.le <hailv2.21it@vku.udn.vn>
2024-06-25 10:32:50 +0700

    task crud User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, Length } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsEmail()
+    email : string;
+
+    @IsNotEmpty()
+    @Length(10, 20, {
+        message : "The minimum of password is 10 character and maximum is 20"
+    })
+    password : string;
+    
+    @IsNotEmpty()
+    name : string;
+
+    address : string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import mongoose, { Model } from 'mongoose';
+import {genSaltSync, hashSync} from 'bcryptjs'
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  getHashPassword = (password : string) => {
+  const salt = genSaltSync(10);
+  const hash = hashSync(password, salt);
+  return hash;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const hashPassword = this.getHashPassword(createUserDto.password)
+   let user = await this.userModel.create({
+      email : createUserDto.email,
+      password : hashPassword,
+      name : createUserDto.name
+   })
+    return user;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    if(!mongoose.Types.ObjectId.isValid((id)))
+      return 'Not found user'
+    return await this.userModel.findOne({
+      _id : id
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne({_id : id}, {...updateUserDto});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+     if(!mongoose.Types.ObjectId.isValid((id)))
+      return 'Unavaible Id user'
+    return await this.userModel.deleteOne({_id : id});
   }
 }
