2532afe1f91b726d04025d0f27f7c292ee5f2f6c
hai.le <hailv2.21it@vku.udn.vn>
2024-06-26 21:51:21 +0700

    Task Auth JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,18 +2,32 @@ import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/commo
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private comfigService : ConfigService
+    private comfigService : ConfigService,
+    private authService : AuthService
+
   
   ) {}
-
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
-    return req.user
+    return this.authService.login(req.user);
   }
+
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+
 }
