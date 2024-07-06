562099b2cb968477323265fba4193a199accb8b2
Raymond <hinh.le@savvycomsoftware.com>
2024-07-06 09:25:54 +0700

    Task CRUD companies

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,29 +4,76 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/user.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class CompaniesService {
 
-constructor(@InjectModel(Company.name) private CompanyModel: SoftDeleteModel<CompanyDocument>) {}
+  constructor(@InjectModel(Company.name) private CompanyModel: SoftDeleteModel<CompanyDocument>) { }
 
-  async create(createCompanyDto: CreateCompanyDto) {
-    return await this.CompanyModel.create({...createCompanyDto});
+  async create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return await this.CompanyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    });
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, projection, population } = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.CompanyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.CompanyModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      // @ts-ignore: Unreachable code error
+      .sort(sort)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems // tổng số phần tử (số bản ghi)
+      },
+      result //kết quả query
+    }
+
   }
 
   findOne(id: number) {
     return `This action returns a #${id} company`;
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUser) {
+    return await this.CompanyModel.updateOne({ _id: id }, {
+      ...updateCompanyDto,
+      updatedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string, user: IUser) {
+    await this.CompanyModel.updateOne({ _id: id }, {
+      deletedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+    return this.CompanyModel.softDelete({ _id: id });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -11,20 +11,20 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
-  const reflector = app.get( Reflector );
-  // app.useGlobalGuards( new JwtAuthGuard( reflector ) );
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-//config CORS
+  //config CORS
   app.enableCors({
-"origin": "*",
-"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-"preflightContinue": false,
-"optionsSuccessStatus": 204
-});
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
